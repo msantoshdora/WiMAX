57aa85346c3feecba2c9d279a52532394dad04d9
Santosh Dora <sanal5hd@gmail.com>
2016-08-09 23:39:49 +0530

    Added Correct functionality of SubscriberStation

--- wimax.cpp
+++ wimax.cpp
@@ -2,29 +2,54 @@
 
 using namespace std;
 
+class Request{
 
-struct request{
+public:
 	string sender;
 	string destination;
 	float datarate;
 	float sinr;  //Signal to noise Ratio
 	int arrivalTime;
 	int deadline;
+ 
+	Request(string s,string d,float datar,float snr,int at,int dl){
+		sender = s;
+		destination = d;
+		datarate = datar;
+		sinr = snr;
+		arrivalTime = at;
+		deadline = dl;		
+	}
 };
+
+/**
+ * To Add a Priority to deadline for the priority
+ */
+bool operator<(const Request& a, const Request& b) {
+    return a.deadline < b.deadline;
+}
 
 class SubscriberStation{
 	private:
 		int numberOfRequests;
+		string s;
+		string d;
+		float dr;
+		float snr;
+		int at;
+		int dl;
 	protected:
-		queue<request> ugs;
-		queue<request> rtps;
-		queue<request> ertps;
-		queue<request> nrtps;
-		queue<request> be;
+		priority_queue<Request> ugs;
+		priority_queue<Request> rtps;
+		priority_queue<Request> ertps;
+		priority_queue<Request> nrtps;
+		priority_queue<Request> be;
 	public:
 		void getCurrentRequest();
 		void getTrafficData(int traffic);
+		friend void display(int r);
 };
+
 
 void SubscriberStation::getCurrentRequest(){
 	for(int i=0;i<5;i++){
@@ -51,24 +76,25 @@ void SubscriberStation::getCurrentRequest(){
 
 
 void SubscriberStation:: getTrafficData(int traffic){
-	request obj;
-	queue<request> temp;
+	int serial=1;
+	priority_queue<Request> temp;
         while(numberOfRequests--){
+		cout<<"Enter the Request for "<<serial<<" SubscriberStation:\n";
 		cout<<"Enter the sender:\n";
-		cin>>obj.sender;
+		cin>>s;
 		cout<<"Enter the receiver:\n";
-		cin>>obj.destination;
-		cout<<"Enter the sinr value:\n";
-		cin>>obj.sinr;
-		cout<"Enter the datarate value:\n";
-		cin>>obj.datarate;
+		cin>>d;
+		cout<<"Enter the datarate value:\n";
+		cin>>dr;
+                 cout<<"Enter the sinr ratio:\n";
+		cin>>snr;
 		cout<<"Enter the arrival time:\n";
-		cin>>obj.arrivalTime;
+		cin>>at;
 		cout<<"Enter the deadline:\n";
-		cin>>obj.deadline;
+		cin>>dl;
 		
-		if(obj.sinr>=5.0){
-		 	temp.push(obj);
+		if(snr>=5.0){
+		 	temp.push(Request(s,d,dr,snr,at,dl));
 		}
 	}	
 
@@ -90,16 +116,116 @@ void SubscriberStation:: getTrafficData(int traffic){
 	}
 	
 }
-//class BaseStation: public SubscriberStation{
-//	public:
-//		 getTrafficRequest();
-//		checkTraffic();
-//		watiRequest();
-//		allocateBandwidth();
-//};
 
+void display(int r){
+ 	priority_queue<Request> obj;
+	switch(r){
+	case 0:obj = ugs;
+		break;
+	case 1:
+		obj = rtps;
+		break;
+	case 2: obj = ertps;
+		break;
+	case 3: obj = nrtps;
+		break;
+	case 4: obj = be;
+		break;
+	}
+	while(!obj.empty()){
+
+		Request t = obj.top();
+		obj.pop();
+		cout<<t.sender<<" "<<t.destination<<endl;
+		
+	}     
+}
+
+class BaseStation{
+	private:
+		int numberOfSubscriberStation;
+		priority_queue<Request> ugs;
+		priority_queue<Request> rtps;
+		priority_queue<Request> ertps;
+		priority_queue<Request> nrtps;
+		priority_queue<Request> be;	
+	public:
+		void getTrafficRequest();
+		void totalSubscriberStation();
+	//	void checkTraffic();
+	//	watRequest();
+	//	allocateBandwidth();
+};
+
+void BaseStation::totalSubscriberStation(){
+	cout<<"Enter number of Subscriber Station:\n";
+	cin>>numberOfSubscriberStation;
+}
+
+void BaseStation:: getTrafficRequest(){
+	SubscriberStation ss1,ss2;
+	priority_queue<Request> s1,s2,temp;
+        int serial = 1;
+	while(numberOfSubscriberStation--){
+		cout<<"Get Traffic Request for: "<<serial<<" station.\n";
+		serial++;
+		SubscriberStation ss;
+		ss.getCurrentRequest();
+	for(int i=0;i<5;i++){
+		switch(i){
+		case 0: temp = ugs;
+			s1  = ss.ugs;
+			break;
+		case 1:
+			temp = rtps;
+			s1 = ss.rtps;
+			break;
+		case 2: 
+			temp = ertps;
+			s1 = ss.ertps;
+			break;
+		case 3:
+			temp = nrtps;
+			s1 = ss.nrtps;
+			break;
+		case 4: 
+			temp = be;
+			s1 = ss.be;
+			break; 
+		}
+
+		while(!s1.empty()){
+			Request t;
+			t = s1.top();
+			s1.pop();
+			temp.push(t);
+	
+		}
+		
+		switch(i){
+		 case 0: ugs = temp;
+			break;
+		 case 1: rtps = temp;
+			break;
+	       	 case 2: ertps = temp;
+			break;
+		 case 3: nrtps = temp;
+			break;
+		case 4: be = temp;
+			break;
+		}
+     }		
+}
 
 int main(){
-  SubscriberStation obj;
+ // SubscriberStation ss1,ss2;
+ // ss1.getCurrentRequest();
+ // ss2.getCurrentRequest();
+BaseStation bs;
+bs.getTrafficRequest();
+cout<<"To display the Traffic(choose 0-ugs,1 for rtPS,2-ertPS,3,nrtPS,4-BE):\n";
+  int r;
+cin>>r;
+bs.display(r);
   return 0;
 }

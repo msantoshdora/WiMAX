b5aa6cd1a47a01a2582653991cee9ab4843a34c2
Santosh Dora <sanal5hd@gmail.com>
2016-08-11 01:18:32 +0530

    Displaying all the Request as per Near Distance

--- wimax.cpp
+++ wimax.cpp
@@ -11,14 +11,22 @@ public:
 	float sinr;  //Signal to noise Ratio
 	int arrivalTime;
 	int deadline;
- 
-	Request(string s,string d,float datar,float snr,int at,int dl){
+        int type;
+	float  wt;
+	float near;
+	float score;
+       
+	Request(){}
+	Request(string s,string d,float datar,float snr,int at,int dl,int ty){
 		sender = s;
 		destination = d;
 		datarate = datar;
 		sinr = snr;
 		arrivalTime = at;
 		deadline = dl;		
+		type = ty;
+		near = sinr*type;
+		//wt = 0.5*()/();
 	}
 };
 
@@ -26,7 +34,7 @@ public:
  * To Add a Priority to deadline for the priority
  */
 bool operator<(const Request& a, const Request& b) {
-    return a.deadline < b.deadline;
+    return a.near < b.near;
 }
 
 class SubscriberStation{
@@ -38,35 +46,61 @@ class SubscriberStation{
 		float snr;
 		int at;
 		int dl;
-	protected:
+		int requestType;
+	public:
 		priority_queue<Request> ugs;
 		priority_queue<Request> rtps;
 		priority_queue<Request> ertps;
 		priority_queue<Request> nrtps;
 		priority_queue<Request> be;
-	public:
+	        
 		void getCurrentRequest();
 		void getTrafficData(int traffic);
-		friend void display(int r);
+	        void display(int r);
+		void setRequestType(int r);
+		int getRequestType();
 };
 
+int SubscriberStation::getRequestType(){
+	return requestType;
+}
+
+void SubscriberStation::setRequestType(int r){
+	switch(r){
+	case 0: requestType = 10;
+		break;
+	case 1: requestType = 9;
+		break;
+	case 2: requestType = 8;
+		break;
+	case 3: requestType = 6;
+		break;
+	case 4: requestType = 3;
+		break;
+	}
+}
 
 void SubscriberStation::getCurrentRequest(){
 	for(int i=0;i<5;i++){
 		switch(i){
 		case 0: 
 			cout<<"Enter the number of ugs Requests:\n";
+			setRequestType(i);
 			break;
 		case 1:
 			cout<<"Enter the number of rtps Requests:\n";
+			setRequestType(i);
 			break;
 		case 2:
 			cout<<"Enter the number of ertps Requests:\n";
+			setRequestType(i);
 			break;
 		case 3:
 			cout<<"Enter the number of nrtps Requests:\n";
+			setRequestType(i);
 			break;
 		case 4: cout<<"Enter the number of Best Effort Requests:\n";
+			setRequestType(i);
 			break;	
 		}
 		cin>>numberOfRequests;
@@ -94,7 +128,7 @@ void SubscriberStation:: getTrafficData(int traffic){
 		cin>>dl;
 		
 		if(snr>=5.0){
-		 	temp.push(Request(s,d,dr,snr,at,dl));
+		 	temp.push(Request(s,d,dr,snr,at,dl,getRequestType()));
 		}
 	}	
 
@@ -117,7 +151,7 @@ void SubscriberStation:: getTrafficData(int traffic){
 	
 }
 
-void display(int r){
+void SubscriberStation::display(int r){
  	priority_queue<Request> obj;
 	switch(r){
 	case 0:obj = ugs;
@@ -149,13 +183,85 @@ class BaseStation{
 		priority_queue<Request> ertps;
 		priority_queue<Request> nrtps;
 		priority_queue<Request> be;	
+		priority_queue<Request> pendingRequest;
 	public:
 		void getTrafficRequest();
 		void totalSubscriberStation();
+		void display(int r);
+		priority_queue<Request> getRequest(priority_queue<Request> r,priority_queue<Request> temp);
+		void getPendingRequest();
+		void displayPendingRequest();
 	//	void checkTraffic();
-	//	watRequest();
+	//	float watRequest();
 	//	allocateBandwidth();
 };
+
+
+void BaseStation::displayPendingRequest(){
+	priority_queue<Request> temp;
+	temp = pendingRequest;
+	while(!temp.empty()){
+		Request t;
+		t = temp.top();
+		cout<<"Sender: "<<t.sender<<" Receiver: "<<t.destination<<"Near: "<<t.score<<"Deadline: "<<t.deadline<<endl;
+		temp.pop();
+	}	
+}
+
+void BaseStation::getPendingRequest(){
+	for(int i=0;i<5;i++){
+		if(i==0){
+			pendingRequest = getRequest(pendingRequest,ugs);
+		}
+		if(i==1){
+			pendingRequest = getRequest(pendingRequest,rtps);
+		}	
+		if(i==2){
+			pendingRequest = getRequest(pendingRequest,ertps);
+		}
+		if(i ==3){
+			pendingRequest = getRequest(pendingRequest,nrtps);
+		}
+		if(i==4){
+			pendingRequest = getRequest(pendingRequest,be);
+		}
+	}
+}
+
+priority_queue<Request> BaseStation::getRequest(priority_queue<Request> r,priority_queue<Request> temp){
+
+		while(!temp.empty()){
+			Request t = temp.top();
+			temp.pop();
+			r.push(t);	
+		}
+return r;
+}
+
+void BaseStation::display(int r){
+priority_queue<Request> obj;
+	switch(r){
+	case 0:obj = ugs;
+		break;
+	case 1:
+		obj = rtps;
+		break;
+	case 2: obj = ertps;
+		break;
+	case 3: obj = nrtps;
+		break;
+	case 4: obj = be;
+		break;
+	}
+	while(!obj.empty()){
+
+		Request t = obj.top();
+		obj.pop();
+		cout<<t.sender<<" "<<t.destination<<" "<<t.near<<endl;
+		
+	}     
+	
+}
 
 void BaseStation::totalSubscriberStation(){
 	cout<<"Enter number of Subscriber Station:\n";
@@ -163,37 +269,39 @@ void BaseStation::totalSubscriberStation(){
 }
 
 void BaseStation:: getTrafficRequest(){
-	SubscriberStation ss1,ss2;
+	totalSubscriberStation();
+//	SubscriberStation ss1,ss2;
 	priority_queue<Request> s1,s2,temp;
         int serial = 1;
 	while(numberOfSubscriberStation--){
-		cout<<"Get Traffic Request for: "<<serial<<" station.\n";
+		cout<<"Get Traffic Request for "<<serial<<" station.\n";
 		serial++;
 		SubscriberStation ss;
 		ss.getCurrentRequest();
 	for(int i=0;i<5;i++){
-		switch(i){
-		case 0: temp = ugs;
+	switch(i){
+		case 0: 
+//  			temp = ugs;
 			s1  = ss.ugs;
 			break;
 		case 1:
-			temp = rtps;
+//			temp = rtps;
 			s1 = ss.rtps;
 			break;
 		case 2: 
-			temp = ertps;
+//			temp = ertps;
 			s1 = ss.ertps;
 			break;
 		case 3:
-			temp = nrtps;
+//			temp = nrtps;
 			s1 = ss.nrtps;
 			break;
 		case 4: 
-			temp = be;
+//			temp = be;
 			s1 = ss.be;
 			break; 
 		}
-
+/*
 		while(!s1.empty()){
 			Request t;
 			t = s1.top();
@@ -202,30 +310,32 @@ void BaseStation:: getTrafficRequest(){
 	
 		}
 		
-		switch(i){
-		 case 0: ugs = temp;
+*/		switch(i){
+		 case 0: ugs = getRequest(ugs,s1);
 			break;
-		 case 1: rtps = temp;
+		 case 1: rtps = getRequest(rtps,s1);
 			break;
-	       	 case 2: ertps = temp;
+	       	 case 2: ertps = getRequest(ertps,s1);
 			break;
-		 case 3: nrtps = temp;
+		 case 3: nrtps = getRequest(nrtps,s1);
 			break;
-		case 4: be = temp;
+		case 4: be = getRequest(be,s1);
 			break;
 		}
-     }		
+	
+     }	
+//	ss.display(0);	
+}
+
+
 }
 
 int main(){
- // SubscriberStation ss1,ss2;
- // ss1.getCurrentRequest();
- // ss2.getCurrentRequest();
+ 
 BaseStation bs;
 bs.getTrafficRequest();
-cout<<"To display the Traffic(choose 0-ugs,1 for rtPS,2-ertPS,3,nrtPS,4-BE):\n";
-  int r;
-cin>>r;
-bs.display(r);
+bs.getPendingRequest();
+//bs.display(0);
+bs.displayPendingRequest();
   return 0;
 }
